d6c074843b37ab6c29e2b21574c3dae3dcbf9766
Tina K <tinakuo77@gamil.com>
2021-01-23 10:26:45 -0600

    Financial Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,9 @@
 # create a Python script that analyzes the records to calculate each of the following:
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
+# 1. The total number of months included in the dataset
+# 2. The net total amount of "Profit/Losses" over the entire period
+# 3. Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# 4. The greatest increase in profits (date and amount) over the entire period
+# 5. The greatest decrease in losses (date and amount) over the entire period
 
 # ------------------------------------------------------
 # Import dependencies
