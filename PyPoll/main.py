50efb0ae4798acf289e9d6b31a64feb012f5b8dd
Tina K <tinakuo77@gamil.com>
2021-01-23 10:25:08 -0600

    Financial Analysis

---
+++ PyPoll/main.py
@@ -0,0 +1,6 @@
+# Create a Python script that analyzes the votes and calculates each of the following:
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
