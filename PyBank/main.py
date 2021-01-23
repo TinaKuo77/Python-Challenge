6efb358b437b705d146ffd286825335cbc71b681
Tina K <tinakuo77@gamil.com>
2021-01-23 02:57:27 -0600

    Financial Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,11 +12,21 @@ import csv
 #path to financial data
 pybank_path_CSV = os.path.join("Resources", "budget_data .csv")
 
+# define variables
+total_month = []
+total_amount = []
+
 #open the file in "read" mode
 with open(pybank_path_CSV) as paybank_csvfile:
-    print(paybank_csvfile)
+   csvreader = csv.reader(paybank_csvfile, delimiter=",")
+   #print(csvreader)
+   csv_header = next(csvreader)
+   #print(f"csv_header:{csv_header}")
+   for row in csvreader:
+       #print(row)
+    total_month.append(row[0])
+    total_amount.append(row[1])
+    print(total_month)
 
 
-total_month = []
-total_amout = []
 
