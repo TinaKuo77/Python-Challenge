df3bac82a5b2f2224f8d28c52374fe9115b5576d
Tina K <tinakuo77@gamil.com>
2021-01-23 11:14:38 -0600

    Financial Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,6 +11,9 @@
 import os
 import csv
 
+number_votes = 0
+
+
 # path to election_data and output file
 pypoll_path_csv = os.path.join("Resources", "election_data.csv")
 pypoll_output_csv = os.path.join("Output", "election_data.text")
@@ -26,3 +29,11 @@ with open(pypoll_path_csv) as pypoll_csvfile:
     # print(csvreader)
     csv_header = next(pypoll_csvfile)
     # print(f"csv_header:{csv_header}")
+    for row in csvreader:
+        # print(row)
+        candidates.append(row[2])
+        candidates_list = sorted(candidates)
+        print(candidates_list)
+    #     votes_each_candidate.append(candidates_list:row[1])
+    # total_number_candidates = (candidates_list)
+    # print(candidates_list)
