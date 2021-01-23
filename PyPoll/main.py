a8d6376164a09a780c08b1ade2d4a5edaa8dd916
Tina K <tinakuo77@gamil.com>
2021-01-23 10:52:45 -0600

    Financial Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,3 +14,15 @@ import csv
 # path to election_data and output file
 pypoll_path_csv = os.path.join("Resources", "election_data.csv")
 pypoll_output_csv = os.path.join("Output", "election_data.text")
+
+# Define pypoll variables
+candidates = []
+votes_each_candidate = []
+
+
+# open the file in "read" mode
+with open(pypoll_path_csv) as pypoll_csvfile:
+    csvreader = csv.reader(pypoll_csvfile, delimiter=",")
+    # print(csvreader)
+    csv_header = next(pypoll_csvfile)
+    # print(f"csv_header:{csv_header}")
