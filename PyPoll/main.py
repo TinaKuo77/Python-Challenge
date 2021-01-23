481341d534a4ef6783f6481a30eb73cf055f1c5e
Tina K <tinakuo77@gamil.com>
2021-01-23 11:26:01 -0600

    Financial Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,29 +11,30 @@
 import os
 import csv
 
-number_votes = 0
-
-
 # path to election_data and output file
 pypoll_path_csv = os.path.join("Resources", "election_data.csv")
 pypoll_output_csv = os.path.join("Output", "election_data.text")
 
 # Define pypoll variables
-candidates = []
+candidates_list = []
 votes_each_candidate = []
-
+number_votes = 0
 
 # open the file in "read" mode
 with open(pypoll_path_csv) as pypoll_csvfile:
     csvreader = csv.reader(pypoll_csvfile, delimiter=",")
     # print(csvreader)
-    csv_header = next(pypoll_csvfile)
+    csv_header = next(csvreader)
     # print(f"csv_header:{csv_header}")
+
+    # count total number of votes cast
     for row in csvreader:
-        # print(row)
-        candidates.append(row[2])
-        candidates_list = sorted(candidates)
+        number_votes = number_votes + 1
+        candidates_list.append(row[2])
+        candidates_list = sorted(candidates_list)
         print(candidates_list)
-    #     votes_each_candidate.append(candidates_list:row[1])
+    # for candidate in candidates_list:
+
+        # votes_each_candidate.append(candidates_list:row[1])
     # total_number_candidates = (candidates_list)
     # print(candidates_list)
