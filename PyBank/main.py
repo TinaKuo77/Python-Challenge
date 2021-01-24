341f05ce4d97d30f9665cea7e34b96fb24bfe747
Tina K <tinakuo77@gamil.com>
2021-01-23 19:33:49 -0600

    Financial Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,83 +12,70 @@ import csv
 
 #path to financial data and output 
 pybank_path_csv = os.path.join("Resources", "budget_data .csv")
-pybank_output_csv = os.path.join("Output", "budget_data.text")
+pybank_output_csv = os.path.join("Output", "budget_data.txt")
+
+# create a list
+month = []
+profit = []
+monthly_change = []
 
 # define variables
-total_month = []
-total_amount = []
-
-count_month = 0
-net_profit_loss = 0
-previous_month_profit_loss = 0
-current_month_profit_loss = 0
-profit_losses_change = 0
+count = 0
+inital_profit = 0
+total_change_profit= 0
+total_profit = 0
 
 
 #open the file in "read" mode
 with open(pybank_path_csv) as pybank_csvfile:
    csvreader = csv.reader(pybank_csvfile, delimiter=",")
-   #print(csvreader)
+#    print(csvreader)
    csv_header = next(pybank_csvfile)
-   #print(f"csv_header:{csv_header}")
+#    print(f"csv_header:{csv_header}")
+       
    for row in csvreader:
-       #print(row)
-     count_month += 1
-     current_month_profit_loss = int(row[1])
-     net_profit_loss += current_month_profit_loss
+        # print(row)
+        count = count + 1
+        month.append(row[0])
+        profit.append(row[1])
+        total_profit = inital_profit + int(row[1])
 
-    #  print(current_month_profit_loss)
-    #  print(count_month)
-    #  print(net_profit_loss)
+        final_profit = int(row[1])
+        monthly_change_profit = inital_profit - final_profit 
+        monthly_change.append(monthly_change_profit)
+        
+        total_change_profit = total_change_profit + monthly_change_profit
 
-if (count_month == 1):
-    previous_month_profit_loss = current_month_profit_loss
-else:
-    profit_losses_change = current_month_profit_loss - previous_month_profit_loss
-    total_month.append(row[0])
-    total_amount.append(row[1])
-    #reset
-    previous_month_profit_loss = current_month_profit_loss
-  
-    # print(count_month)
-    # print(profit_losses_change)
-    # print(net_profit_loss)
+        inital_profit = final_profit
 
-# The average of "Profit/Losses" over the entire period
-    average = round(net_profit_loss/(count_month -1), 2)
-    # print(average)
-
-# The greatest increase/decrease in profits (date and amount) over the entire period
-    greatest_increase = max(total_amount)
-    greatest_decrease = min(total_amount)
-    # print(greatest_increase)
-    # print(greatest_decrease)
-# Locate the index value of increase/decrease
-    greatest_increase_month_index = total_amount.index(greatest_increase)
-    greatest_decrease_month_index = total_amount.index(greatest_decrease)
-
-# Assign greatest increase/decrease month
-    greatest_increase_month = total_month[greatest_increase_month_index]
-    greatest_decrease_month = total_month[greatest_decrease_month_index]
+        # Calculate the average change
+        average_change_profit = (total_change_profit/count)
+        
+        # Find the max and min change in profits and its month
+        Max_profit_change = max(monthly_change)
+        Min_profit_change = min(monthly_change) 
+            
+        Max_profit_change_month = month[monthly_change.index(Max_profit_change)]
+        Min_profit_change_month = month[monthly_change.index(Min_profit_change)]
 
 # Print the analysis
 print("Financial Analysis")
 print("----------------------------")
-print(f"Total Months:  {count_month}")
-print(f"Total:  ${net_profit_loss}")
-print(f"Average Change:  ${average}")
-print(f"Greatest Increase in Profits:  {greatest_increase_month} (${greatest_increase_month_index})")
-print(f"Greatest Decrease in Losses:  {greatest_decrease_month} (${greatest_decrease_month_index})")
+print(f"Total Months:  {count}")
+print(f"Total:  ${total_profit}")
+print(f"Average Change:  ${average_change_profit}")
+print(f"Greatest Increase in Profits:  {Max_profit_change_month} (${Max_profit_change})")
+print(f"Greatest Decrease in Losses:  {Min_profit_change_month} (${Min_profit_change})")
 
 # Export a text file
-pybank_output_csv = os.path.join("Output", "budget_data.text")
+pybank_output_csv = os.path.join("Output", "budget_data.txt")
 with open(pybank_output_csv, "w") as text_file:
             text_file.write(
                 f"Financial Analysis: \n"
                 f"----------------------------\n"
-                f"Total Months: {count_month}\n" 
-                f"Total : ${net_profit_loss}\n"
-                f"Average Change: ${average}\n"
-                f"Greatest Increase in Profit: {greatest_increase_month} (${greatest_increase_month_index})\n"
-                f"Greatest Decrease in Losses: {greatest_decrease_month} (${greatest_decrease_month_index})\n"
-            )
+                f"Total Months: {count}\n" 
+                f"Total : ${total_profit}\n"
+                f"Average Change: ${average_change_profit}\n"
+                f"Greatest Increase in Profit: {Max_profit_change_month} (${Max_profit_change})\n"
+                f"Greatest Decrease in Losses: {Min_profit_change_month} (${Min_profit_change})\n"
+                )
